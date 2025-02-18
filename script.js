ba756bb7faf82f9685a8a60f9b4e5e74c1256bcb
Fmdeveloper <fmdeveloper2@gmail.com>
2025-02-18 10:21:38 -0300

    create repository

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcular() {
+  var comissao = (parseFloat($("#valorPorcentagem").val()) / 100) * parseFloat($("#valor_vendedor").val());
+  $("#comissao").val(comissao);
+  $("#sera_pago").val($("#valor_vendedor").val() - $("#comissao").val())
+}
+
