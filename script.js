2a3215e375925ca7546cf789cc6022c4105ee594
Fmdeveloper <fmdeveloper2@gmail.com>
2025-02-18 09:45:15 -0300

    create repository

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 
-  function calcularComissao() {
-    var valor = document.getElementById("botao").value;
-    var comissao = valor * 0.08;
-    alert("A comissão é de R$ " + comissao);
-  }
-  calcularComissao()
 
